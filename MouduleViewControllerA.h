60feec98527757a38103d8f757fe2972a0beade3
codeXKT <184722567@qq.com>
2021-11-18 14:23:19 +0800

    传入一个测试moudule

---
+++ MouduleViewControllerA.h
@@ -0,0 +1,16 @@
+//
+//  MouduleViewControllerA.h
+//  testHello
+//
+//  Created by 谢开涛 on 2021/11/18.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MouduleViewControllerA : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
